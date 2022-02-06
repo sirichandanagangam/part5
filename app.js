4fc7c76f54c7387e5b287cee537b967013648ad0
amulyaAmmu <40600002+amulyaAmmu@users.noreply.github.com>
2022-02-05 23:21:52 -0800

    4th bullet

--- app.js
+++ app.js
@@ -35,13 +35,18 @@ function post_data(request, response){
   let passwords = ["123", "456", "789"]
   var username = request.body.username
   var pwd = request.body.password
-  console.log(req.body)
+  console.log(request.body)
   if (username != "" && usernames.includes(username)){
     if(pwd != "" && passwords.includes(pwd)){
       
-      var token = "Abc_1234"
+      var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+      
+      var res_json = {
+        "access-token" : token,
+        "expires" : new Date().toISOString()
+      }
       console.log("validated")
-      return token
+      response.json(res_json)
       
     }
   }
@@ -55,7 +60,6 @@ function post_data(request, response){
   
   
 }
-
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
