c1408599aa2c1396ed4749b5c60a0eb29a73a72c
sirichandanagangam <97136285+sirichandanagangam@users.noreply.github.com>
2022-02-06 15:01:17 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const port = 3000
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var apiMocker = require('connect-api-mocker');
+const resp = require("./response.json")
+app.get('/data/2.5/weather', (req, res) => {
+  console.log("hi")
+  res.send(resp);    
+})
+
+app.get('/v1/weather', get_weather_v1)
+
+app.get('/v1/hello', get_hello)
+
+app.post('/v1/auth', post_data)
+
+
+function get_weather_v1(request, response){
+  var token1 = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+  
+  if(request.body.token == token1){
+    
+    response.send(resp)
+  }
+  else{
+    console.log("Token Not Matching")
+  }
+}
+
+function get_hello(request, response){
+  const authHeader = request.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+
+  const token2 = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+  console.log(request.body)
+  if(token == "") response.sendStatus(401)
+  if(token == token2){
+    response.json({ "message" : "Hi! I am Amulya"})
+  }
+  else{
+    console.log("Token Not Matching")
+    response.sendStatus(403)
+  }
+  
+}
+
+function post_data(request, response){
+  
+  let usernames = ["Amulya", "Siri", "Pavan"]
+  let passwords = ["123", "456", "789"]
+  var username = request.body.username
+  var pwd = request.body.password
+  console.log(request.body)
+  if (username != "" && usernames.includes(username)){
+    if(pwd != "" && passwords.includes(pwd)){
+      
+      var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+      
+      var res_json = {
+        "access_token" : token,
+        "expires" : new Date().toISOString()
+      }
+      console.log("validated")
+      response.json(res_json)
+      
+    }
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
